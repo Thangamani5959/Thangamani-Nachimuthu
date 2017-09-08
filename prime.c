b2b149087ad9b2f98a23e28442bc0773d41aa4aa
Thangamani5959 <31482682+Thangamani5959@users.noreply.github.com>
2017-09-08 19:14:52 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,count=0;
+scanf("%d",&a);
+for(i=2;i=a/2,i<a;i++)
+{
+count=count+1;
+}
+if(count>2)
+{
+printf("not prime");
+}
+else
+{
+printf("not prime")'
+}
+return 0;
+}
