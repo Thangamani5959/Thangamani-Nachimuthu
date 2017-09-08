8f10e0759f14ad47898467fd362ffe1af4db5996
Thangamani5959 <31482682+Thangamani5959@users.noreply.github.com>
2017-09-08 22:41:44 +0530

    Create primeinterval.c

---
+++ primeinterval.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+scanf("%d/n%d",&a,&b);
+if(a>b)
+{
+for(i=a;i<=b,i=a/2,i++)
+{
+count=count+1;
+printf(
+}
+}
+else
+{
+for(i=b;i<=a,i=b/2,i++)
+{
+count=count+1;
+}
+}
+if(count<2)
+{
+printf("prime");
+}
+else
+{
+printf("not prime");
+}
+return 0;
+}
