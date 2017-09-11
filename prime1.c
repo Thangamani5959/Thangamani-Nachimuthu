585d30c8d4762f7b109d2f0d620cb99103748241
Thangamani5959 <31482682+Thangamani5959@users.noreply.github.com>
2017-09-11 17:54:56 +0530

    Update and rename prime.c to prime1.c

--- prime1.c
+++ prime1.c
@@ -3,17 +3,19 @@ int main()
 {
 int a,count=0;
 scanf("%d",&a);
-for(i=2;i=a/2,i<a;i++)
+for(i=1;,i<=a;i++)
 {
+  if(a%i==0)
+  {
 count=count+1;
-}
+}}
 if(count>2)
 {
 printf("not prime");
 }
 else
 {
-printf("not prime")'
+printf("prime")'
 }
 return 0;
 }
